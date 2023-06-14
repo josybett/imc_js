b6ba4383d0244ff7810f064704491ecfb3a687ec
Josy <camposjosybett@gmail.com>
2023-06-13 23:36:51 -0500

    pre entrega 3

---
+++ js/calculo_imc.js
@@ -0,0 +1,168 @@
+//fórmula: peso (kg) / [estatura (m)]2
+// Cálculo de Índice de Masa Muscular
+
+class IMC {
+
+    constructor (nombre, edad, peso, estatura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.estatura = estatura;
+        this.imc = 0;
+    }
+
+    // METODOS
+    validar_datos() {
+        return (!isNaN(this.peso) && !isNaN(this.estatura));
+    }
+
+    calculo_imc() {
+        let validar = !this.validar_datos() ? false : Math.round(this.peso / (this.estatura * this.estatura));
+        return validar;
+    }
+}
+
+class RecomendacionesIMC {
+    resultado;
+    indicacion;
+
+    constructor () {
+        this.resultado = "";
+        this.indicacion = "";
+    }
+}
+
+function calculo_imprimir(persona) {
+    let imc = persona.calculo_imc();
+    if (imc != false) {
+        persona.imc = imc;
+    }
+    return persona;
+}
+
+function interpretar_imc(imc) {
+    let recomendacion = new RecomendacionesIMC();
+    if (imc < 18.5) {
+        recomendacion.resultado = "Bajo peso";
+        recomendacion.indicacion = "Aumenta la ingesta de proteína, frutas y vegetales";
+    } else if (imc >= 18.5 && imc < 25) {
+        recomendacion.resultado = "Peso Normal";
+        recomendacion.indicacion = "No olvides la actividad física";
+    } else if (imc >= 25 && imc < 30) {
+        recomendacion.resultado = "Sobrepeso";
+        recomendacion.indicacion = "Prioriza la actividad física y reduce los carbohidratos";
+    } else {
+        recomendacion.resultado = "Obesidad";
+        recomendacion.indicacion = "Cuida de no comer grasas y muy pocos carbohidratos de cerelaes. Haz ejercicio a diario";
+    }
+    return recomendacion;
+}
+
+
+const form_calcular = document.getElementById("form_calcular");
+const inp_edad = document.getElementById("inp_edad");
+const inp_peso = document.getElementById("inp_peso");
+const inp_estatura = document.getElementById("inp_estatura");
+const inp_buscar_persona = document.getElementById("inp_buscar_persona");
+const btn_buscar_persona = document.getElementById("btn_buscar_persona");
+const div_resultado = document.getElementById("resultado");
+const div_lista_filtro = document.getElementById("lista_filtro");
+
+let error_inputs = false;
+let nombre_usuario = "";
+
+function validacion_numerica({target}) {
+    const {value, name} = target;
+    if (parseInt(value) && parseInt(value) > 0 && !value.includes("e")) {
+        switch (name) {
+            case "edad":
+                document.getElementById("span_edad").innerHTML = '';
+                error_inputs = false;
+                break;
+            case "peso":
+                document.getElementsByName("span_peso").innerHTML = '';
+                error_inputs = false;
+                break;
+            case "estatura":
+                document.getElementsByName("span_estatura").innerHTML = '';
+                error_inputs = false;
+                break;
+            default:
+                break;
+        }
+        return;
+    }
+    const text_error = 'Sólo valores numéricos positivos';
+    switch (name) {
+        case "edad":
+            document.getElementById("span_edad").innerHTML = text_error + ' enteros';
+            error_inputs = true;
+            break;
+        case "peso":
+            document.getElementById("span_peso").innerHTML = text_error + ' decimal con punto';
+            error_inputs = true;
+            break;
+        case "estatura":
+            document.getElementById("span_estatura").innerHTML = text_error + ' decimal con punto';
+            error_inputs = true;
+            break;
+        default:
+            break;
+    }
+}
+
+function btn_calcular_imc(event) {
+    event.preventDefault();
+    if (!error_inputs) {
+        const nombre = document.getElementById("inp_nombre").value;
+        const edad = inp_edad.value;
+        const peso = inp_peso.value;
+        const estatura = inp_estatura.value;
+
+        console.log(edad);
+        let persona = new IMC(nombre.trim().toUpperCase(), parseInt(edad), parseFloat(peso), parseFloat(estatura));
+        let imc = calculo_imprimir(persona);
+        div_resultado.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    <strong>Registro exitoso!</strong> ${imc.nombre}, el resultado del cálculo de IMC es: ${imc.imc} </br>
+                                    Para más información, filtra por tu nombre.
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>`;
+        let dataStorage = JSON.parse(localStorage.getItem("arr_personas") || '[]');
+        let arreglo_JSON = JSON.stringify([...dataStorage, imc]);
+        localStorage.setItem("arr_personas" , arreglo_JSON);
+    }
+}
+
+function filtro_person_nombre(obj_imc) {
+    return obj_imc.nombre == nombre_usuario.toUpperCase();
+}
+
+function buscar_persona() {
+    let arr = localStorage.getItem("arr_personas");
+    nombre_usuario = inp_buscar_persona.value;
+    arr = JSON.parse(arr);
+    let arr_filter = nombre_usuario == "" ? arr : arr.filter(filtro_person_nombre);
+
+    div_lista_filtro.innerHTML = "";
+
+    for( let person of arr_filter ){
+        let { nombre, edad, peso, estatura, imc } = person;
+        let { resultado, indicacion } = interpretar_imc(imc);
+        let lista = document.createElement("ul");
+        lista.classList.add("list-group", "list-group-horizontal");
+        lista.innerHTML = `<li class="list-group-item">${nombre}</li>
+                        <li class="list-group-item">Edad ${edad} años</li>
+                        <li class="list-group-item">Peso ${peso} kg</li>
+                        <li class="list-group-item">Estatura ${estatura} m</li>
+                        <li class="list-group-item">IMC ${imc} - ${resultado}</li>
+                        <li class="list-group-item">Recomendaciones: ${indicacion}</li>`;
+        div_lista_filtro.append(lista);
+    }
+}
+
+
+inp_edad.addEventListener("change", validacion_numerica);
+inp_peso.addEventListener("change", validacion_numerica);
+inp_estatura.addEventListener("change", validacion_numerica);
+form_calcular.addEventListener("submit", btn_calcular_imc);
+btn_buscar_persona.addEventListener("click", buscar_persona);
